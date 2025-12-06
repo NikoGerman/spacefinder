dec39bedd70d2ea084794b4774516ea01aa3c64a
Nikolai German <129080480+NikoGerman@users.noreply.github.com>
2025-12-06 16:35:02 +0100

    tests for augment()

--- tests/testthat/test-augment_LearnerSubspacePolygon.R
+++ tests/testthat/test-augment_LearnerSubspacePolygon.R
@@ -1,10 +1,10 @@
-test_that("multiplication works", {
+test_that("basic tests", {
   data("benchmark_data")
   task <- as_task_subspace(
     benchmark_data,
     auc ~ (learning_rate + max_depth) * optimizer
   )
-  learner <- LearnerSubspaceBox$new(task)
+  learner <- LearnerSubspacePolygon$new(task)
   learner$train()
   aug <- suppressWarnings(augment(learner))
   expect_true(inherits(aug, "data.table"))
